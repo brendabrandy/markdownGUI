4c9e248965bec2b09f06ed28df27c43733e3dff9
brendabrandy <so@cooper.edu>
2016-05-29 23:30:07 -0400

    Added editText Box

--- main.py
+++ main.py
@@ -9,15 +9,20 @@ class App:
     self.bQuit.pack(side=LEFT)
     self.bHeaders = Button(frame,
 			text = "Headers",
-			command=self.write_slogan)
+			command=self.write_slogan("Head"))
     self.bEmphasis = Button(frame,
                          text="Emphasis",
-                         command=self.write_slogan)
+                         command=self.write_slogan("Emph"))
+    self.listbox = Listbox(frame)
     self.bQuit.pack(side=TOP)
     self.bHeaders.pack(side=TOP)
     self.bEmphasis.pack(side=TOP)
-  def write_slogan(self):
-    print "Tkinter is easy to use!"
+    self.listbox.pack(side=RIGHT)
+  def write_slogan(self,type):
+    if (type == "Head"):
+	self.listbox.insert(END,"Head")
+    elif (type == "Emph"):
+	self.listbox.insert(END,"Emph")
 
 root = Tk()
 app = App(root)
