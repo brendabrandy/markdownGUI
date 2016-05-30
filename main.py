3cf04fac5e1e7610bb91d140d1cf6bb0a4e2d63b
brendabrandy <so@cooper.edu>
2016-05-30 00:05:09 -0400

    Able to add textboses

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from Tkinter import *
+
+
+def write_slogan(myType):
+    if (myType == "Head"):
+	t = Text(root)
+	t.pack()
+    else:
+	print "Other stuff"
+
+
 class App:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
         self.bQuit = Button(frame, text="QUIT", fg="red",command=frame.quit)
         self.bQuit.pack(side=LEFT)
-        self.bHeaders = Button(frame,text = "Headers",command=lambda: self.write_slogan("Head"))
-        self.bEmphasis = Button(frame,text="Emphasis",command=lambda: self.write_slogan("Emph"))
+        self.bHeaders = Button(frame,text = "Headers",command=lambda: write_slogan("Head"))
+        self.bEmphasis = Button(frame,text="Emphasis",command=lambda: write_slogan("Emph"))
         self.listbox = Listbox(frame)
         self.bQuit.pack(side=TOP)
         self.bHeaders.pack(side=TOP)
         self.bEmphasis.pack(side=TOP)
         self.listbox.pack(side=RIGHT)
-    def write_slogan(self,myType):
-        if (myType == "Head"):
-	    self.listbox.insert(END,"Head")
-	elif (myType == "Emph"):
-	    self.listbox.insert(END,"Emph")
 
 root = Tk()
 app = App(root)
 root.mainloop()
+
+	
